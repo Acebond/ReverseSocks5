a8990b2b47e86e275e32ae50510f36c69f8400ef
unknown <6915160+Acebond@users.noreply.github.com>
2023-02-09 18:11:56 +1000

    added proxy support

--- go.mod
+++ go.mod
@@ -6,3 +6,12 @@ require (
 	github.com/hashicorp/yamux v0.1.1
 	github.com/things-go/go-socks5 v0.0.2
 )
+
+require (
+	github.com/magisterquis/connectproxy v0.0.0-20200725203833-3582e84f0c9b // indirect
+	github.com/mattn/go-ieproxy v0.0.9 // indirect
+	golang.org/x/exp v0.0.0-20230206171751-46f607a40771 // indirect
+	golang.org/x/net v0.0.0-20220630215102-69896b714898 // indirect
+	golang.org/x/sys v0.1.0 // indirect
+	golang.org/x/text v0.3.7 // indirect
+)
--- main.go
+++ main.go
@@ -3,58 +3,125 @@ package main
 import (
 	"crypto/tls"
 	"flag"
+	"fmt"
 	"io"
 	"log"
 	"net"
+	"net/url"
+	"os"
+	"strings"
 
 	"github.com/hashicorp/yamux"
+	"github.com/magisterquis/connectproxy"
 	"github.com/things-go/go-socks5"
+	"golang.org/x/net/proxy"
 )
+
+func Usage(serverCmd, agentCmd *flag.FlagSet) {
+	fmt.Printf("Usage: ReverseSocks5 server [flags]\n")
+	serverCmd.PrintDefaults()
+	fmt.Println()
+	fmt.Printf("Usage: ReverseSocks5 agent [flags]\n")
+	agentCmd.PrintDefaults()
+	fmt.Println()
+	os.Exit(1)
+}
 
 func main() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 
 	var (
-		listen   = flag.String("listen", "", "listen address for socks agents address:port")
-		cert     = flag.String("cert", "", "certificate file")
-		key      = flag.String("key", "", "private key file")
-		socks    = flag.String("socks", "127.0.0.1:1080", "listen address for socks server address:port")
-		connect  = flag.String("connect", "", "connect address for socks agent address:port")
-		insecure = flag.Bool("k", false, "Allow insecure server connections")
+		serverCmd = flag.NewFlagSet("server", flag.ExitOnError)
+		listen    = serverCmd.String("listen", ":10443", "Listen address for socks agents address:port")
+		cert      = serverCmd.String("cert", "CreateCertificate", "Certificate filepath for the socks server")
+		key       = serverCmd.String("key", "GenerateKey", "Private key filepath for the socks server")
+		socks     = serverCmd.String("socks", "127.0.0.1:1080", "Listen address for socks server address:port")
+		agentCmd  = flag.NewFlagSet("agent", flag.ExitOnError)
+		connect   = agentCmd.String("connect", "", "Connect address for socks agent address:port")
+		proxy     = agentCmd.String("proxy", "", "Proxy address for the socks agent schema://address:port. Supports http/https/socks5.")
+		insecure  = agentCmd.Bool("k", false, "Allow insecure server connections on the socks agent")
 	)
-	flag.Parse()
 
-	if *listen != "" {
-		var cer tls.Certificate
-		var err error
-		if *cert != "" && *key != "" {
-			cer, err = tls.LoadX509KeyPair(*cert, *key)
-		} else {
-			log.Println("Generating a self-signed X.509 certificate")
-			cer, err = MakeCert()
-		}
-		if err != nil {
-			log.Fatalln(err.Error())
-		}
-		log.Println("Listening for socks agents/clients")
-		ListenForAgent(*listen, *socks, cer)
-	} else if *connect != "" {
-		log.Println("Connecting to socks server")
-		ReverseSocksAgent(*connect, *insecure)
-	} else {
-		flag.PrintDefaults()
+	if len(os.Args) < 2 {
+		Usage(serverCmd, agentCmd)
+	}
+
+	switch os.Args[1] {
+	case serverCmd.Name():
+		serverCmd.Parse(os.Args[2:])
+		ReverseSocksServer(*listen, *cert, *key, *socks)
+	case agentCmd.Name():
+		agentCmd.Parse(os.Args[2:])
+		ReverseSocksAgent(*connect, *proxy, *insecure)
+	default:
+		Usage(serverCmd, agentCmd)
 	}
 }
 
-// Start a socks5 server and tunnel the traffic to the server at address.
-func ReverseSocksAgent(address string, insecure bool) {
-	config := &tls.Config{InsecureSkipVerify: insecure}
-	conn, err := tls.Dial("tcp", address, config)
+func ReverseSocksServer(listen, cert, key, socks string) {
+	var cer tls.Certificate
+	var err error
+	if cert != "CreateCertificate" && key != "GenerateKey" {
+		cer, err = tls.LoadX509KeyPair(cert, key)
+	} else {
+		log.Println("Generating a self-signed X.509 certificate")
+		cer, err = MakeCert()
+	}
 	if err != nil {
 		log.Fatalln(err.Error())
 	}
-	log.Println("Connected")
+	log.Println("Listening for socks agents on " + listen)
+	ListenForAgent(listen, socks, cer)
+}
 
+func ConnectUsingProxy(proxyString, address string) (net.Conn, error) {
+	proxy.RegisterDialerType("http", connectproxy.New)
+	proxy.RegisterDialerType("https", connectproxy.New)
+
+	proxyURL, err := url.Parse(proxyString)
+	if nil != err {
+		return nil, err
+	}
+
+	d, err := proxy.FromURL(proxyURL, proxy.Direct)
+	if nil != err {
+		return nil, err
+	}
+	c, err := d.Dial("tcp", address)
+	return c, err
+}
+
+// Start a socks5 server and tunnel the traffic to the server at address.
+func ReverseSocksAgent(address, proxyString string, insecure bool) {
+	tlsConfig := &tls.Config{InsecureSkipVerify: insecure}
+
+	var conn *tls.Conn
+	var err error
+
+	log.Println("Connecting to socks server at " + address)
+
+	if proxyString == "" {
+		conn, err = tls.Dial("tcp", address, tlsConfig)
+		if err != nil {
+			log.Println(err.Error())
+			return
+		}
+	} else {
+		log.Println("Using proxy " + proxyString)
+		proxiedConn, err := ConnectUsingProxy(proxyString, address)
+		if err != nil {
+			log.Println(err.Error())
+			return
+		}
+		tlsConfig.ServerName = strings.Split(address, ":")[0]
+		conn = tls.Client(proxiedConn, tlsConfig)
+	}
+
+	log.Println("Connected")
+	AgentRun(conn)
+}
+
+func AgentRun(conn net.Conn) {
 	session, err := yamux.Server(conn, nil)
 	if err != nil {
 		log.Fatalln(err.Error())
@@ -86,6 +153,7 @@ func ReverseSocksAgent(address string, insecure bool) {
 
 // Accepts connections and tunnels the traffic to the SOCKS server running on the client.
 func TunnelServer(listen string, session *yamux.Session) {
+	log.Println("Listening for socks clients on " + listen)
 	ln, err := net.Listen("tcp", listen)
 	if err != nil {
 		log.Fatalln(err.Error())
